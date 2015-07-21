198a3e001daf9196ed91f18e43fd63b3ac3df299
Chris Finch <chris.finch@guardian.co.uk>
2015-07-21 15:20:22 +0100

    log

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ var SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/T07K8BAMP/B07USMSES/gK
 
 var responseToRequest = function(req, res, next) {
 
+    console.log(req);
+
     var URL_REGEX = /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
     var url = req.body && req.body.text ? req.body.text.match(URL_REGEX)[0] : false;
