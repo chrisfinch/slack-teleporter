12d52c4a2b72078a08d3582f3a880167e2aef4a5
Chris Finch <chris.finch@guardian.co.uk>
2015-07-21 12:46:41 +0100

    port

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/teleport', function(req, res, next) {
 
-  console.log('request');
+  console.log('request', req);
 
 });
 
 router.post('/teleport', function(req, res, next) {
 
-  console.log('request');
+  console.log('request', req);
 
 });
 
